d0c51a7d50473f21a560c7b1a95921ed1a5547a0
Zaedin <brunoasnunes98@gmail.com>
2026-09-17 12:39:10 +0100

    Add config file Allow changing how temperature and light levels affect grow

---
+++ SubZeroFarming/FarmingOverrides.cs
@@ -0,0 +1,9 @@
+﻿namespace SubZeroFarming;
+
+public class FarmingOverrides
+{
+    public int DelayGrowthBelowSunLight = 19;
+    public float DelayGrowthBelowTemperature = 10f;
+    public float LossPerDegree = 0.1f;
+    public float LossPerLevel = 0.1f;
+}
--- SubZeroFarming/SubZeroFarmingModSystem.cs
+++ SubZeroFarming/SubZeroFarmingModSystem.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Reflection;
-using System.Reflection.Emit;
-using HarmonyLib;
-using Vintagestory.API.Common;
+﻿using Vintagestory.API.Common;
 using Vintagestory.API.Server;
 using Vintagestory.GameContent;
 
@@ -11,7 +7,6 @@ namespace SubZeroFarming;
 public class SubZeroFarmingModSystem : ModSystem
 {
     private const string HarmonyId = "SubZeroFarming"; // Use a unique ID
-    private Harmony? _harmony;
 
     public override bool ShouldLoad(EnumAppSide forSide)
     {
@@ -20,65 +15,30 @@ public class SubZeroFarmingModSystem : ModSystem
 
     public override void StartServerSide(ICoreServerAPI api)
     {
-        base.StartServerSide(api);
+        base.Start(api);
+        
+        var config = api.LoadModConfig<FarmingOverrides>("subzerofarming.json");
 
-        _harmony = new Harmony(HarmonyId);
-
-        var original = AccessTools.Method(typeof(BlockEntityFastForwardGrowth), "Update");
-        var transpiler = new HarmonyMethod(
-            typeof(SubZeroFarmingModSystem).GetMethod(nameof(Transpiler),
-                BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public));
-
-        _harmony.Patch(original, transpiler: transpiler);
-        var info = Harmony.GetPatchInfo(
-            AccessTools.Method(typeof(BlockEntityFastForwardGrowth), "Update"));
-
-        if (info.Transpilers.Count > 0)
-            api.Logger.Notification($"[{_harmony.Id}] Patch applied successfully. Crops should now grow below 0ºC.");
-        else
-            api.Logger.Error($"[{_harmony.Id}] Patch not applied. Please send your logs to the dev.");
-    }
-
-    public override void Dispose()
-    {
-        _harmony?.UnpatchAll(HarmonyId);
-        base.Dispose();
-    }
-
-    [HarmonyPatch(typeof(BlockEntityFastForwardGrowth), "Update", typeof(float))]
-    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-    {
-        var list = new List<CodeInstruction>(instructions);
-        FileLog.Log("Patching BlockEntityFastForwardGrowth");
-
-        for (var i = 0; i < list.Count - 1; i++)
+        if (config == null)
         {
-            var opcodeName = list[i].opcode.Name;
-            if (opcodeName == null)
-                continue;
-
-            if (!opcodeName.StartsWith("ldloc"))
-                continue;
-
-            if (list[i + 1].opcode != OpCodes.Callvirt)
-                continue;
-
-            if (list[i + 1].operand is not MethodInfo mi)
-                continue;
-
-            if (mi.Name != "Invoke")
-                continue;
-
-            var p = mi.GetParameters();
-            if (p.Length != 4 || p[3].ParameterType != typeof(bool))
-                continue;
-
-            // switch "growthPaused" with "false"
-            list[i].opcode = OpCodes.Ldc_I4_0;
-            list[i].operand = null;
-            break;
+            config = new FarmingOverrides();
+            api.StoreModConfig(config, "subzerofarming.json");
         }
+        
+        api.Event.ServerRunPhase(EnumServerRunPhase.RunGame, () =>
+        {
+            var farming = api.ModLoader.GetModSystem<ModSystemFarming>();
 
-        return list;
+            if (farming?.Config == null)
+            {
+                api.Logger.Error("ModSystemFarming config not found");
+                return;
+            }
+
+            farming.Config.DelayGrowthBelowTemperature = config.DelayGrowthBelowTemperature;
+            farming.Config.LossPerDegree = config.LossPerDegree;
+            farming.Config.DelayGrowthBelowSunLight = config.DelayGrowthBelowSunLight;
+            farming.Config.LossPerLevel = config.LossPerLevel;
+        });
     }
 }
